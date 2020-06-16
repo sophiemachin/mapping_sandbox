cb3c14e9b2ff86b2763136dc0388ef72fa3ebcb5
sem <sem@pearson.co.uk>
2020-06-16 11:55:32 +0100

    folium_chloropleth: used cleaned GB json

--- folium_chloropleth.py
+++ folium_chloropleth.py
@@ -1,19 +1,19 @@
 import folium
 import pandas as pd
 
-url = 'https://raw.githubusercontent.com/python-visualization/folium/master/examples/data'
-state_geo = f'{url}/us-states.json'
-state_unemployment = f'{url}/US_Unemployment_Oct2012.csv'
-state_data = pd.read_csv(state_unemployment)
+
+uk_geo = 'result.json'
+gb_data_csv = f'GB_data.csv'
+gb_data = pd.read_csv(gb_data_csv)
 
 m = folium.Map(location=[48, -102], zoom_start=3)
 
 folium.Choropleth(
-    geo_data=state_geo,
+    geo_data=uk_geo,
     name='choropleth',
-    data=state_data,
-    columns=['State', 'Unemployment'],
-    key_on='feature.id',
+    data=gb_data,
+    columns=['objectid', 'Amount'],
+    key_on='id',
     fill_color='YlGn',
     fill_opacity=0.7,
     line_opacity=0.2,
